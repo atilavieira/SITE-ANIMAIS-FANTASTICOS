37c6c67baeb6a8601f14ea6e675edc333d5fc614
Atila vieira <atilavieiralopes@hotmail.com>
2022-12-08 23:11:59 -0300

    Modal add

--- js/script.js
+++ js/script.js
@@ -2,13 +2,11 @@ import initScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
-import * as teste from './modules/teste.js';
-
-teste.teste1()
-teste.teste2()
+import initmodal from './modules/modal.js';
 
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
+initmodal()
